a5027cf2675065b9ce37712f75fe462d08c882ec
officialtreytrizzy-lab <officialtreytrizzy@gmail.com>
2026-06-28 08:04:43 -0500

    Route mobile OpenCut studio

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
+import MobileStudio from "./pages/MobileStudio";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,6 +20,8 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/studio" element={<MobileStudio />} />
+            <Route path="/opencut" element={<MobileStudio />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
