4fbc659d1f042cc4c9485a48c937cc19bc2194a6
officialtreytrizzy-lab <officialtreytrizzy@gmail.com>
2026-06-28 11:11:48 -0500

    Add Trey Video API health endpoint

---
+++ api/v1/health.ts
@@ -0,0 +1,15 @@
+import { describeAuthSetup } from '../_lib/auth';
+import { handleOptions, json } from '../_lib/http';
+import { modalBaseUrl } from '../_lib/modal';
+
+export default async function handler(req: any, res: any) {
+  if (handleOptions(req, res)) return;
+  json(res, 200, {
+    service: 'Trey Video API',
+    version: 'v1',
+    status: 'ok',
+    modal_worker_configured: Boolean(modalBaseUrl()),
+    products: ['video_removal', 'video_editor', 'opencut_sdk'],
+    auth: describeAuthSetup(),
+  });
+}
