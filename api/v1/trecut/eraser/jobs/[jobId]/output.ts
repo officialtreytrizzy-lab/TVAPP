7232e8aa8a746763f607a0ddc91918df4e1b8533
officialtreytrizzy-lab <officialtreytrizzy@gmail.com>
2026-07-05 19:37:15 -0500

    Keep rollback eraser output route NodeNext compatible

--- api/v1/trecut/eraser/jobs/[jobId]/output.ts
+++ api/v1/trecut/eraser/jobs/[jobId]/output.ts
@@ -1,5 +1,5 @@
-import { error, handleOptions, methodNotAllowed } from '../../../../../_lib/http';
-import { fetchTreyVideoRemovalApi } from '../../../../../_lib/trecut-eraser-proxy';
+import { error, handleOptions, methodNotAllowed } from '../../../../../_lib/http.js';
+import { fetchTreyVideoRemovalApi } from '../../../../../_lib/trecut-eraser-proxy.js';
 
 export default async function handler(req: any, res: any) {
   if (handleOptions(req, res)) return;
